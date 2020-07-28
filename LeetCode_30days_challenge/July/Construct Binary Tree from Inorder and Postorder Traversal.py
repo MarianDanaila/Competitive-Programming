435441339265382f6f8c2580548dc76be955ed18
Danaila Marian <mariandanaila01@gmail.com>
2020-07-28 13:56:42 +0300

    A small change in code

--- LeetCode_30days_challenge/July/Construct Binary Tree from Inorder and Postorder Traversal.py
+++ LeetCode_30days_challenge/July/Construct Binary Tree from Inorder and Postorder Traversal.py
@@ -13,11 +13,12 @@ class Solution:
         return root
 
 # Time-complexity:O(n)
-class Solution:
+class Solution2:
     def buildTree(self, inorder, postorder):
         map_inorder = {}
-        for i, val in enumerate(inorder):
-            map_inorder[val] = i
+        for i in range(len(inorder)):
+            map_inorder[inorder[i]] = i
+        print(map_inorder)
         def recur(low, high):
             if low > high:
                 return None
@@ -27,3 +28,6 @@ class Solution:
             x.left = recur(low, mid-1)
             return x
         return recur(0, len(inorder)-1)
+
+sol = Solution2()
+print(sol.buildTree([9,3,15,20,7], [9,15,7,20,3]))
