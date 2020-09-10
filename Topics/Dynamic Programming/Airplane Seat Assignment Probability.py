b75d4c34d4e18e192e56cdfebdd8a92485217307
Danaila Marian <mariandanaila01@gmail.com>
2020-09-11 00:16:54 +0300

    DP problem

---
+++ Topics/Dynamic Programming/Airplane Seat Assignment Probability.py
@@ -0,0 +1,7 @@
+# Read explanation of winterock
+class Solution:
+    def nthPersonGetsNthSeat(self, n: int) -> float:
+        if n == 1:
+            return 1
+        else:
+            return 0.50000
