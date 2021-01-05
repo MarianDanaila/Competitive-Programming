d94479691d810eabb20331663a5f86be94ef050c
Danaila Marian <mariandanaila01@gmail.com>
2021-01-05 08:12:13 +0200

    Minimum Spanning Tree Problem

--- Topics/Graphs/Min Cost to Connect All Points.py
+++ Topics/Graphs/Min Cost to Connect All Points.py
@@ -1,3 +1,4 @@
+# Approach 1: Kruskal's Algorithm
 class Solution:
     def minCostConnectPoints(self, points):
         n = len(points)
@@ -36,4 +37,42 @@ class Solution:
                 min_cost += w
                 union(set_u, set_v)
             i += 1
+        return min_cost
+
+
+# Approach 2: Prim's Algorithm
+import heapq
+
+
+class Solution:
+    def minCostConnectPoints(self, points):
+        graph = {}
+        n = len(points)
+        if n == 1:
+            return 0
+        for i in range(n - 1):
+            point1 = points[i]
+            for j in range(i + 1, n):
+                point2 = points[j]
+                manhattan = abs(point1[0] - point2[0]) + abs(point1[1] - point2[1])
+                if i not in graph:
+                    graph[i] = [[j, manhattan]]
+                else:
+                    graph[i].append([j, manhattan])
+
+                if j not in graph:
+                    graph[j] = [[i, manhattan]]
+                else:
+                    graph[j].append([i, manhattan])
+        visited = set()
+        min_cost = 0
+        min_heap = [[0, 0]]
+        while min_heap:
+            cost, point = heapq.heappop(min_heap)
+            if point not in visited:
+                visited.add(point)
+                min_cost += cost
+                for nxt, cost in graph[point]:
+                    if nxt not in visited:
+                        heapq.heappush(min_heap, [cost, nxt])
         return min_cost
