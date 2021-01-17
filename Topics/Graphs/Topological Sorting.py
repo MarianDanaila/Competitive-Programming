b41115c6e830160abdf81ed459fe25057bf2b908
Danaila Marian <mariandanaila01@gmail.com>
2021-01-17 18:08:49 +0200

    Add Kahn Algorithm

--- Topics/Graphs/Topological Sorting.py
+++ Topics/Graphs/Topological Sorting.py
@@ -1,3 +1,4 @@
+# Approach 1 using modified DFS
 nr_vertices, nr_edges = [int(s) for s in input().split(" ")]
 graph = {}
 for _ in range(nr_edges):
@@ -23,3 +24,36 @@ for i in range(nr_vertices):
     if not visited[i]:
         topological_sort(i, visited, stack)
 print(stack[::-1])
+
+# Approach 2 using Kahn's method
+from collections import deque
+nr_vertices, nr_edges = [int(s) for s in input().split(" ")]
+in_degree = [0] * nr_vertices
+graph = {}
+for _ in range(nr_edges):
+    v1, v2 = [int(s) for s in input().split(" ")]
+    in_degree[v2] += 1
+    if v1 not in graph:
+        graph[v1] = [v2]
+    else:
+        graph[v1].append(v2)
+
+dq = deque()
+visited = 0
+for i in range(len(in_degree)):
+    if in_degree[i] == 0:
+        dq.append(i)
+
+while dq:
+    curr = dq.popleft()
+    visited += 1
+    if curr in graph:
+        for nxt in graph[curr]:
+            in_degree[nxt] -= 1
+            if in_degree[nxt] == 0:
+                dq.append(nxt)
+if visited != nr_vertices:
+    print("No topological sorting because cycle is found")
+else:
+    print("There exist a topological sorting")
+
