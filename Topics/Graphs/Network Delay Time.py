d38bdcddf78debedfbaa695c0fd49f5eeb9ad414
Danaila Marian <mariandanaila01@gmail.com>
2021-01-18 10:38:26 +0200

    Add Bellman Ford & Floyd Warshall

--- Topics/Graphs/Network Delay Time.py
+++ Topics/Graphs/Network Delay Time.py
@@ -1,6 +1,9 @@
+# Dijkstra
 import heapq
+
+
 class Solution:
-    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+    def networkDelayTime(self, times, n, k):
         graph = {}
         for u, v, w in times:
             if u not in graph:
@@ -18,5 +21,44 @@ class Solution:
                         heapq.heappush(min_heap, [time+w, nxt])
         if max(dist) < float("inf"):
             return max(dist)
+        else:
+            return -1
+
+
+# Bellman Ford
+class Solution:
+    def networkDelayTime(self, times, n, k):
+        dist = [float("inf")] * n
+        dist[k-1] = 0
+        for _ in range(n-1):
+            for u, v, w in times:
+                u -= 1
+                v -= 1
+                if dist[u] + w < dist[v]:
+                    dist[v] = dist[u] + w
+
+        if max(dist) < float("inf"):
+            return max(dist)
+        else:
+            return -1
+
+
+# Floyd Warshall
+class Solution:
+    def networkDelayTime(self, times, n, k):
+        dist = [[float("inf")] * n for _ in range(n)]
+        for i in range(n):
+            dist[i][i] = 0
+
+        for u, v, w in times:
+            dist[u-1][v-1] = w
+
+        for a in range(n):
+            for i in range(n):
+                for j in range(n):
+                    dist[i][j] = min(dist[i][j], dist[i][a]+dist[a][j])
+
+        if max(dist[k-1]) < float("inf"):
+            return max(dist[k-1])
         else:
             return -1
