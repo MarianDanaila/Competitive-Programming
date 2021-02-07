44b444c0cf7b15ad8bf04564a0723b0cbcc32099
Danaila Marian <mariandanaila01@gmail.com>
2021-02-07 22:50:45 +0200

    Stack, Design

---
+++ Topics/Queue & Stack/Min Stack.py
@@ -0,0 +1,31 @@
+class MinStack:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+
+    def push(self, x: int) -> None:
+        if not self.stack:  # if stack is empty
+            self.stack.append([x, x])
+        else:
+            minim = self.stack[-1][1]
+            minim = min(minim, x)
+            self.stack.append([x, minim])
+
+    def pop(self) -> None:
+        self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1][0]
+
+    def getMin(self) -> int:
+        return self.stack[-1][1]
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(x)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
