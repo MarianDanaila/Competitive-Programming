29953c678628cbfb41b7dda3a204cbe73afc3022
Danaila Marian <mariandanaila01@gmail.com>
2021-05-12 11:05:16 +0300

    DP

---
+++ LeetCode_30days_challenge/2021/May/Range Sum Query 2D - Immutable.py
@@ -0,0 +1,35 @@
+from typing import List
+
+
+class NumMatrix:
+
+    def __init__(self, matrix: List[List[int]]):
+        rows = len(matrix)
+        cols = len(matrix[0])
+        self.range_sum = [[0] * cols for _ in range(rows)]
+        self.range_sum[0][0] = matrix[0][0]
+        for col in range(1, cols):
+            self.range_sum[0][col] = self.range_sum[0][col - 1] + matrix[0][col]
+
+        for row in range(1, rows):
+            self.range_sum[row][0] = self.range_sum[row - 1][0] + matrix[row][0]
+
+        for row in range(1, rows):
+            for col in range(1, cols):
+                self.range_sum[row][col] = matrix[row][col] + self.range_sum[row - 1][col] + self.range_sum[row][
+                    col - 1] - self.range_sum[row - 1][col - 1]
+
+    def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
+        if row1 == 0 and col1 == 0:
+            return self.range_sum[row2][col2]
+        if row1 == 0:
+            return self.range_sum[row2][col2] - self.range_sum[row2][col1 - 1]
+        if col1 == 0:
+            return self.range_sum[row2][col2] - self.range_sum[row1 - 1][col2]
+
+        return self.range_sum[row2][col2] - self.range_sum[row1 - 1][col2] - self.range_sum[row2][col1 - 1] + \
+            self.range_sum[row1 - 1][col1 - 1]
+
+# Your NumMatrix object will be instantiated and called as such:
+# obj = NumMatrix(matrix)
+# param_1 = obj.sumRegion(row1,col1,row2,col2)
