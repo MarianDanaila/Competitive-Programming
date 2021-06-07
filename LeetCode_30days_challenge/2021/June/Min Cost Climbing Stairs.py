150bb9c2b8e7654a63cf6fb18398428241bc2290
Danaila Marian <mariandanaila01@gmail.com>
2021-06-07 20:23:46 +0300

    Array, DP

---
+++ LeetCode_30days_challenge/2021/June/Min Cost Climbing Stairs.py
@@ -0,0 +1,9 @@
+from typing import List
+
+
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        first = second = 0
+        for c in cost:
+            first, second = second, c + min(first, second)
+        return min(first, second)
