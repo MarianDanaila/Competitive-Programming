e642ed4e441bf1e053dd2b05f83b47298ec914a9
Danaila Marian <mariandanaila01@gmail.com>
2021-07-23 12:49:01 +0300

    Add DFS Solution

--- Topics/Queue & Stack/Number of Islands.py
+++ Topics/Queue & Stack/Number of Islands.py
@@ -1,6 +1,8 @@
 from collections import deque
 from typing import List
 
+# BFS Approach
+
 
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
@@ -10,7 +12,7 @@ class Solution:
         islands = 0
         for i in range(m):
             for j in range(n):
-                if grid[i][j] == '1':  # run bfs
+                if grid[i][j] == '1':
                     islands += 1
                     dq.append([i, j])
                     grid[i][j] = '-1'
@@ -35,4 +37,37 @@ class Solution:
                             if grid[x][y + 1] == '1':
                                 grid[x][y + 1] = '-1'
                                 dq.append([x, y + 1])
+        return islands
+
+# DFS Approach
+
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        visited = set()
+
+        def dfs(x, y):
+            if x - 1 >= 0 and (x - 1, y) not in visited and grid[x - 1][y] == '1':
+                visited.add((x - 1, y))
+                dfs(x - 1, y)
+            if y + 1 < m and (x, y + 1) not in visited and grid[x][y + 1] == '1':
+                visited.add((x, y + 1))
+                dfs(x, y + 1)
+            if x + 1 < n and (x + 1, y) not in visited and grid[x + 1][y] == '1':
+                visited.add((x + 1, y))
+                dfs(x + 1, y)
+            if y - 1 >= 0 and (x, y - 1) not in visited and grid[x][y - 1] == '1':
+                visited.add((x, y - 1))
+                dfs(x, y - 1)
+
+        n = len(grid)
+        m = len(grid[0])
+        islands = 0
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j] == '1' and (i, j) not in visited:
+                    visited.add((i, j))
+                    islands += 1
+                    dfs(i, j)
+
         return islands
