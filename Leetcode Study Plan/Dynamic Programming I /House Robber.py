630dac9af0a7cfa10ecb0c019aedbf69b583f25c
MarianDanaila <mariandanaila01@gmail.com>
2021-08-23 20:37:51 +0300

    Array, DP

---
+++ Leetcode Study Plan/Dynamic Programming I /House Robber.py
@@ -0,0 +1,26 @@
+from typing import List
+
+
+# Approach 1: Space complexity: O(N), where N is the length of the input array
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n == 1:
+            return nums[0]
+        dp = [0] * n
+        dp[0], dp[1] = nums[0], max(nums[0], nums[1])
+        for i in range(2, n):
+            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i])
+        return dp[-1]
+
+
+# Approach2: Space complexity: O(1)
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n == 1:
+            return nums[0]
+        first, second = nums[0], max(nums[0], nums[1])
+        for i in range(2, n):
+            first, second = second, max(second, first + nums[i])
+        return second
